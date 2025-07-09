0e8a0b533206a827aeddafec5b40eeefebbea7e0
Nick <semenov_nick@mail.ru>
2025-07-09 14:34:28 +0300

    Feedback V3 fix

--- src/core/state/Feedbacks/Items/V3/PlaydeckFeedbacksItemsV3.ts
+++ src/core/state/Feedbacks/Items/V3/PlaydeckFeedbacksItemsV3.ts
@@ -26,8 +26,8 @@ export const PlaydeckFeedbacksDefinitionsV3 = (state: PlaydeckStateV3): Companio
 					label: 'Playlist',
 					default: '1',
 					choices: [
-						{ id: 0, label: 'Left Playlist' },
-						{ id: 1, label: 'Right Playlist' },
+						{ id: 1, label: 'Left Playlist' },
+						{ id: 2, label: 'Right Playlist' },
 					],
 				},
 				{
@@ -47,31 +47,79 @@ export const PlaydeckFeedbacksDefinitionsV3 = (state: PlaydeckStateV3): Companio
 			],
 
 			callback: async (feedback, context): Promise<boolean> => {
+				if (!state) return false
+
 				const options = feedback.options as CheckStateOptionValues
-				const status = state.status.getValues()
-				if (!status) return false
-
-				const playlist = status.channel[Number(options.playlist)]
-				const isState = playlist.state === feedback.options.state
-				if (!isState) return false
-
+				let isState = false
+				let isClip = false
+				let isBlock = false
+				const fState = options.state
+				const fChannel = options.playlist
 				const fClip = await context.parseVariablesInString(options.clip.toString())
 				const fBlock = await context.parseVariablesInString(options.block.toString())
+				const isName = isNaN(Number(fClip)) || isNaN(Number(fBlock))
+				const isAnyClip = Number(fClip) === 0
+				const isAnyBlock = Number(fBlock) === 0
+				const isAny = isAnyClip && isAnyBlock
 
-				const isAny = Number(fClip) === 0 && Number(fBlock) === 0
+				if (state.lastState && isName === false) {
+					const lastStateChannels = state.lastState.playlist
+					if (lastStateChannels) {
+						const lastChannelState = lastStateChannels[Number(fChannel)]
+						if (lastChannelState) {
+							const lastChannelBlock = [String(lastChannelState.blockName), String(lastChannelState.blockNumber)]
+							const lastChannelClip = [String(lastChannelState.clipName), String(lastChannelState.clipNumber)]
+							if (fState !== undefined) {
+								isState = String(lastChannelState.state) === String(fState)
+							}
+
+							if (fBlock !== undefined) {
+								isBlock = lastChannelBlock.indexOf(String(fBlock)) > -1
+							}
+							if (fClip !== undefined) {
+								isClip = lastChannelClip.indexOf(String(fClip)) > -1
+							}
+							if (isAny) return isState
+							if (isAnyBlock || isAnyClip) return (isAnyBlock && isState && isClip) || (isAnyClip && isState && isBlock)
+							return isState && isClip && isBlock
+						}
+					}
+				}
+
+				if (state.status === null || state.status === undefined) return false
+
+				const status = state.status.getValues()
+
+				if (status === null || status === undefined) return false
+
+				const stateChannels = status.channel
+
+				if (stateChannels === null || stateChannels === undefined) return false
+
+				const stateChannel = stateChannels[Number(fChannel) - 1]
+
+				if (stateChannel === null || stateChannel === undefined) return false
+
+				if (fState !== undefined) {
+					isState = String(stateChannel.state) === String(fState)
+					if (isState === false) return false
+				}
+
+				if (String(stateChannel.state) === String(PlaybackState.Stop)) return isState
+
+				const stateChannelBlock = [String(stateChannel.blockName), String(stateChannel.blockID)]
+				const stateChannelClip = [String(stateChannel.clipName), String(stateChannel.blockID)]
+
+				if (fBlock !== undefined) {
+					isBlock = stateChannelBlock.indexOf(String(fBlock)) > -1
+				}
+				if (fClip !== undefined) {
+					isClip = stateChannelClip.indexOf(String(fClip)) > -1
+				}
+
 				if (isAny) return isState
-
-				const blockID = playlist.blockID
-				const blockName = playlist.blockName
-				const isBlock = [`${blockID}`, `${blockName}`].indexOf(fBlock) > -1
-				const clipID = playlist.clipID
-				const clipName = playlist.clipName
-				const isClip = [`${clipID}`, `${clipName}`].indexOf(fClip) > -1
-				if (Number(fBlock) === 0) return isClip && isState
-
-				if (Number(fClip) === 0) return isBlock && isState
-
-				return isClip && isBlock && isState
+				if (isAnyBlock || isAnyClip) return (isAnyBlock && isState && isClip) || (isAnyClip && isState && isBlock)
+				return isState && isClip && isBlock
 			},
 		},
 	}
